e53620aa3521f7b45b1193ce347977ef59517fb9
3xDaniel <124632083+3xDaniel@users.noreply.github.com>
2023-06-10 13:47:04 -0500

    Update cam_boton.py

--- ser_dan/cam_boton.py
+++ ser_dan/cam_boton.py
@@ -3,56 +3,45 @@ from PIL import Image
 import numpy as np
 
 
-
-cap = cv2.VideoCapture(0)
-
-width = cap.get(3)
-height = cap.get(4)
-
-color = [0,0,255]
-lower_red = np.array([0, 50, 70])
-uppper_red = np.array([9, 255, 255])
-
-def get_limits(color):
-
-    c = np.uint8([[color]])
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-
-    lowerLimit = hsvC[0][0][0] - -160, 50, 50
-    upperLimit = hsvC[0][0][0] + 180, 200, 200
-
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
-
-    return lowerLimit, upperLimit
+#def get_limits(color):
+#
+#    c = np.uint8([[color]])
+#    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+#
+#    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+#    upperLimit = hsvC[0][0][0] + 10, 255, 255
+#
+#    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+#    upperLimit = np.array(upperLimit, dtype=np.uint8)
+#
+#    return lowerLimit, upperLimit
 
 
+def red_box(img):
+	bbox = (0,0,0,0)
+	
+	hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	
+	lower1 = np.array([0, 100, 20])
+	upper1 = np.array([10, 255, 255])
+	lower2 = np.array([160,100,20])
+	upper2 = np.array([179,255,255])
 
-while True:
-    ret, frame = cap.read()
+	lower_mask = cv2.inRange(hsvImage, lower1, upper1)
+	upper_mask = cv2.inRange(hsvImage, lower2, upper2)
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	mask = lower_mask + upper_mask
 
-    lowerLimit, upperLimit = get_limits(color)
-    
-    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-
-    mask_ = Image.fromarray(mask)
-
-    bbox = mask_.getbbox()
-
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        frame = cv2.rectangle(frame, (x1,y1), (x2,y2), (50,50,250), 5)
-
-    cv2.imshow('frame', frame)
-    
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-
+	mask_ = Image.fromarray(mask)
+	bbox = mask_.getbbox()
+	if bbox is not None:
+		x1, y1, x2, y2 = bbox
+		img = cv2.rectangle(img, (x1,y1), (x2,y2), (50,50,250), 5)
+	
+	while True:
+		cv2.imshow ("frame", img)
+		if cv2.waitKey(1) & 0xFF == ord("q"):
+			break
 cap.release()
 
 cv2.destroyAllWindows()
